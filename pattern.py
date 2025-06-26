c206c3606cd0661e8c0fbbf30a803ea74e6c4a66
RuYaV <ruban-owk@yandex.ru>
2025-06-26 13:47:11 +0300

    Update pattern.py

--- pattern.py
+++ pattern.py
@@ -1,31 +1,30 @@
 from tinydb import TinyDB
 
-def database():
-  db = TinyDB('db.json')
-  db.truncate()
+db = TinyDB('db.json')
+db.truncate()
 
-  db.insert({
-    "name": "Данные пользователя",
+db.insert({
+  "name": "Данные пользователя",
+  "login": "email",
+  "tel": "phone"
+})
+db.insert({
+  "name": "Форма заказа",
+  "customer": "text",
+  "order_id": "text",
+  "дата_заказа": "date",
+  "contact": "phone"
+})
+db.insert({
+  "name": "Проба",
+  "f_name1": "email",
+  "f_name2": "date"
+})
+db.insert({
+    "name": "Данные сотрудника",
     "login": "email",
-    "tel": "phone"
-  })
-  db.insert({
-    "name": "Форма заказа",
-    "customer": "text",
-    "order_id": "text",
-    "дата_заказа": "date",
-    "contact": "phone"
-  })
-  db.insert({
-    "name": "Проба",
-    "f_name1": "email",
-    "f_name2": "date"
-  })
-  db.insert({
-      "name": "Данные сотрудника",
-      "login": "email",
-      "phone_number": "phone",
-      "information": "text"
-  })
+    "phone_number": "phone",
+    "information": "text"
+})
 
-  db.close()
+db.close()
